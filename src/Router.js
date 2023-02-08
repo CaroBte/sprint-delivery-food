3a745c0d27be59d0c4a45243cb1d5f6a282ccc4e
CaroBte <karito0721@hotmail.com>
2023-02-07 22:35:31 -0500

    feat: added router.js

---
+++ src/Router.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { createBrowserRouter, Route, createRoutesFromElements } from "react-router-dom"
+import { Navbar } from "./layout";
+
+export const router = createBrowserRouter(
+    createRoutesFromElements(
+        <>
+            <Route path="/" element={<Navbar />} ></Route>
+        </>
+    )
+)
