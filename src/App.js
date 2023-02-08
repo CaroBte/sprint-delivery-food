c161afc1cedcc9b0b329d6c51ababfb430f935aa
CaroBte <karito0721@hotmail.com>
2023-02-07 23:02:43 -0500

    feat: added project title

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import { React } from 'react';
+import { React, useEffect } from 'react';
 import { RouterProvider } from 'react-router-dom';
 import { router } from "./Router";
 
 
 const App = () => {
+
+  useEffect(() => {
+    document.title = "Food Delivery App"
+  }, []);
+
   return (
     <RouterProvider router={router} />
   );
