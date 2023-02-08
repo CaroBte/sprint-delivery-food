6259a2b551ac3a3ef4bf0cd928c2267c11d39a96
CaroBte <karito0721@hotmail.com>
2023-02-07 23:03:54 -0500

    feat: added basic routes

--- src/Router.js
+++ src/Router.js
@@ -1,11 +1,17 @@
 import React from "react";
 import { createBrowserRouter, Route, createRoutesFromElements } from "react-router-dom"
 import { Navbar } from "./layout";
+import { Orders, Profile, Search, Home } from "./pages"
 
 export const router = createBrowserRouter(
     createRoutesFromElements(
         <>
-            <Route path="/" element={<Navbar />} ></Route>
+            <Route path="/" element={<Navbar />} >
+                <Route path="/" element={<Home />} />
+                <Route path="/orders" element={<Orders />} />
+                <Route path="/search" element={<Search />} />
+                <Route path="/profile" element={<Profile />} />
+            </Route>
         </>
     )
 )
--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import { Link, Outlet } from 'react-router-dom'
+
 
 const Navbar = () => {
     return (
-        <div>Navbar</div>
+        <>
+            <h1>Navbar</h1>
+            <ul>
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/search">Search</Link>
+                </li>
+                <li>
+                    <Link to="/orders">Orders</Link>
+                </li>
+                <li>
+                    <Link to="/profile">Profile</Link>
+                </li>
+            </ul>
+            <Outlet />
+        </>
     )
 }
 
