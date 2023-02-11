3492d5729d3db1c3ff7128273ae53a0a3b166360
CaroBte <karito0721@hotmail.com>
2023-02-10 23:34:59 -0500

    feat: deleted test files

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -5,18 +5,17 @@ import { Link, Outlet } from 'react-router-dom'
 const Navbar = () => {
     return (
         <>
-            <h1>Navbar</h1>
-            <ul>
-                <li>
+            <ul className='navbar-nav d-flex flex-row justify-content-evenlynpm s' >
+                <li className='nav-item'>
                     <Link to="/">Home</Link>
                 </li>
-                <li>
+                <li className='nav-item'>
                     <Link to="/search">Search</Link>
                 </li>
-                <li>
+                <li className='nav-item'>
                     <Link to="/orders">Orders</Link>
                 </li>
-                <li>
+                <li className='nav-item'>
                     <Link to="/profile">Profile</Link>
                 </li>
             </ul>
