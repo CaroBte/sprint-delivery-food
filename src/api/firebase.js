d012fd9d9737512c9717925c2e68f2aff7e46df1
CaroBte <karito0721@hotmail.com>
2023-02-10 23:35:16 -0500

    feat: added firebase initial config

---
+++ src/api/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDUgaIvtXRNvDByi2b1acwwkLL0xb9FuGE",
+    authDomain: "sprint4-fda.firebaseapp.com",
+    projectId: "sprint4-fda",
+    storageBucket: "sprint4-fda.appspot.com",
+    messagingSenderId: "448454810640",
+    appId: "1:448454810640:web:2f30f19b1f8e8842aa7e95"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
