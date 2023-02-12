15c2bf7bc405e857bb1a5d61ecaa28d2bb70c3fd
CaroBte <karito0721@hotmail.com>
2023-02-12 14:08:25 -0500

    feat: added sass styles

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { React, useEffect } from 'react';
 import { RouterProvider } from 'react-router-dom';
 import { router } from "./Router";
-
+import "./styles/styles.sass"
 
 const App = () => {
 
--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -5,18 +5,18 @@ import { Link, Outlet } from 'react-router-dom'
 const Navbar = () => {
     return (
         <>
-            <ul className='navbar-nav d-flex flex-row justify-content-evenlynpm s' >
+            <ul id="navbar" className='navbar-nav d-flex flex-row justify-content-around' >
                 <li className='nav-item'>
-                    <Link to="/">Home</Link>
+                    <Link className='nav-link' to="/"><i className="fa-solid fa-house"></i></Link>
                 </li>
                 <li className='nav-item'>
-                    <Link to="/search">Search</Link>
+                    <Link className='nav-link' to="/search"><i className="fa-solid fa-magnifying-glass"></i></Link>
                 </li>
                 <li className='nav-item'>
-                    <Link to="/orders">Orders</Link>
+                    <Link className='nav-link' to="/orders"><i className="fa-solid fa-clock-rotate-left"></i></Link>
                 </li>
                 <li className='nav-item'>
-                    <Link to="/profile">Profile</Link>
+                    <Link className='nav-link' to="/profile"><i className="fa-regular fa-user"></i></Link>
                 </li>
             </ul>
             <Outlet />
