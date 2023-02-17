dd9f7688013013befec0a8eb6aa11b6bb3937ed0
CaroBte <karito0721@hotmail.com>
2023-02-16 21:09:30 -0500

    feat: added user info on profile component

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,20 +1,60 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { useNavigate } from 'react-router'
 import { authApi } from '../api'
+import { auth } from '../context'
 
 const Profile = () => {
 
     const navigate = useNavigate()
+    const { user } = useContext(auth.authContext)
 
     const handleLogout = () => {
         authApi.logout()
         navigate("/")
-
     }
     return (
-        <div>Profile
-            <button onClick={handleLogout} className='btn btn-danger'>Logout</button>
-        </div>
+        <>
+            <div className='align-items-center d-flex flex-column'>
+                <img className='img-profile' src={user.photoURL} alt={user.displayName} />
+                <p className='mt-2'>{user.displayName}</p>
+            </div>
+
+            <div className="profile-items d-flex flex-row align-items-center py-2">
+                <i className="fa-regular fa-user"></i>
+                <p className='text-profile-items'>Account edit</p>
+                <i className="fa-solid fa-angle-right"></i>
+            </div>
+            <div className="profile-items d-flex flex-row align-items-center py-2">
+                <i className="fa-solid fa-wallet"></i>
+                <p className='text-profile-items'>Payment</p>
+                <i className="fa-solid fa-angle-right"></i>
+            </div>
+            <div className="profile-items d-flex flex-row align-items-center py-2">
+                <i className="fa-solid fa-globe"></i>
+                <p className='text-profile-items'>Languaje</p>
+                <span className='eng'>ENG</span>
+
+            </div>
+            <div className="profile-items d-flex flex-row align-items-center py-2">
+                <i className="fa-solid fa-location-dot"></i>
+                <p className='text-profile-items'>Location</p>
+                <i className="fa-solid fa-angle-right"></i>
+            </div>
+            <div className="profile-items d-flex flex-row align-items-center py-2">
+                <i className="fa-regular fa-circle-question"></i>
+                <p className='text-profile-items'>FAQ</p>
+                <i className="fa-solid fa-angle-right"></i>
+            </div>
+            <div className="profile-items d-flex flex-row align-items-center py-2">
+                <i className="fa-solid fa-phone"></i>
+                <p className='text-profile-items'>Support</p>
+                <i className="fa-solid fa-angle-right"></i>
+            </div>
+
+
+            <button onClick={handleLogout} className='btn btn-danger w-75 ms-5'>Logout</button>
+        </>
+
     )
 }
 
